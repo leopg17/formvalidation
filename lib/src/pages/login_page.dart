5e6af036426d835fb5164c78ad369cdac6bc1e8a
Leonardo Porras González <lporras@bolsacr.com>
2020-06-12 22:29:05 -0600

    Primera parte del login

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -4,10 +4,57 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Login Page'),
+      body: Stack(
+        children: <Widget>[
+          _crearFondo(context),
+        ],
       ),
       
+    );
+  }
+
+  Widget _crearFondo(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final fondoMorado = Container(
+      height: size.height * 0.4,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: <Color> [
+            Color.fromRGBO(63, 63, 156, 1.0),
+            Color.fromRGBO(90, 70, 178, 1.0),
+          ]
+        )
+      ),
+    );
+
+    final circulo = Container(
+      width: 70.0,
+      height: 70.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100.0),
+        color: Color.fromRGBO(255, 255, 255, 0.05)
+      ),
+    );
+
+    return Stack(
+      children: <Widget>[
+        fondoMorado,
+        Positioned(top: 90.0, left: 30.0, child: circulo),
+        Positioned(top: -40.0, right: -30.0, child: circulo),
+        Positioned(bottom: 120.0, right: 20, child: circulo),
+        Positioned(bottom: -50.0, left: -20, child: circulo),
+        Container(
+          padding: EdgeInsets.only(top: 80.0),
+          child: Column(
+            children: <Widget>[
+              Icon(Icons.person_pin_circle, color:Colors.white, size: 70),
+              SizedBox(height: 10.0, width: double.infinity,),
+              Text('Leonardo Porras', style: TextStyle(color: Colors.white, fontSize: 25.0),)
+            ],
+          )
+        )
+      ],
     );
   }
 }
