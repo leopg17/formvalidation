f22b9bd3fdaa2e781ca53b7aa74c8fddd9d67576
Leonardo Porras González <lporras@bolsacr.com>
2020-06-23 21:02:09 -0600

    Mantener data despues de un hot reload

--- lib/src/bloc/provider.dart
+++ lib/src/bloc/provider.dart
@@ -4,12 +4,24 @@ export 'package:formvalidation/src/bloc/login_bloc.dart';
 
 class Provider extends InheritedWidget {
 
+  static Provider _instancia;
+
+  factory Provider({Key key, Widget child}){
+    if (_instancia == null){
+      _instancia = new Provider._internal(key: key, child: child);
+    }
+    return _instancia;
+  }
+
+  Provider._internal({Key key, Widget child})
+     :  super(key: key, child: child);
+
   final loginBloc = LoginBloc();
 
 
   //Constructor 
-  Provider({Key key, Widget child})
-    :  super(key: key, child: child);
+  // Provider({Key key, Widget child})
+  //   :  super(key: key, child: child);
 
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) => true;
