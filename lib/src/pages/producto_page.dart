e09dfba46c6c64d373a42df1a2f770b15a57ac1d
Leonardo Porras <leopg17@gmail.com>
2020-06-28 12:46:58 -0600

    Conectar formulario con modelo producto

--- lib/src/pages/producto_page.dart
+++ lib/src/pages/producto_page.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:formvalidation/src/models/producto_model.dart';
 import 'package:formvalidation/src/utils/utils.dart' as utils;
 
-class ProductoPage extends StatelessWidget {
+class ProductoPage extends StatefulWidget {
 
+  @override
+  _ProductoPageState createState() => _ProductoPageState();
+}
+
+class _ProductoPageState extends State<ProductoPage> {
   final formKey = GlobalKey<FormState>();
+
+  ProductoModel producto = new ProductoModel(); 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,6 +39,7 @@ class ProductoPage extends StatelessWidget {
               children: <Widget>[
                 _crearNombre(),
                 _crearPrecio(),
+                _crearDisponible(),
                 _crearBoton(),
               ],
             ),
@@ -42,10 +52,12 @@ class ProductoPage extends StatelessWidget {
 
   Widget _crearNombre() {
     return TextFormField(
+      initialValue: producto.titulo,
       textCapitalization: TextCapitalization.sentences,
       decoration: InputDecoration(
         labelText: 'Producto'
       ),
+      onSaved: (value)=> producto.titulo=value,
       validator: (value){
         if (value.length < 3){
           return 'Ingrese el nombre del producto';
@@ -58,10 +70,12 @@ class ProductoPage extends StatelessWidget {
 
   Widget _crearPrecio() {
     return TextFormField(
+      initialValue: producto.valor.toString(),
       keyboardType: TextInputType.numberWithOptions(decimal: true),
       decoration: InputDecoration(
         labelText: 'Precio'
       ),
+      onSaved: (value)=> producto.valor=double.parse(value),
       validator: (value){
         if(utils.isNumeric(value)){
           return null;
@@ -69,6 +83,17 @@ class ProductoPage extends StatelessWidget {
           return 'Solo números';
         }
       },
+    );
+  }
+
+  _crearDisponible() {
+    return SwitchListTile(
+      value: producto.disponible,
+      title: Text('Disponible'), 
+      activeColor: Colors.deepPurple,
+      onChanged: (value)=> setState((){
+        producto.disponible = value;
+      }),
     );
   }
 
@@ -87,7 +112,11 @@ class ProductoPage extends StatelessWidget {
 
   void _submit(){
     if( !formKey.currentState.validate()) return;
+    formKey.currentState.save();
     print('Todo OK!');
+    print(producto.titulo);
+    print(producto.valor);
+    print(producto.disponible);
     
   }
 }
