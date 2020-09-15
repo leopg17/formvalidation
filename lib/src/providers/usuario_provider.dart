2c87f8724a5b4cb25e61ac714420d370289b17c5
Leonardo Porras González <lporras@bolsacr.com>
2020-09-15 16:22:49 -0600

    Grabar token en el storage

--- lib/src/providers/usuario_provider.dart
+++ lib/src/providers/usuario_provider.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 
+import 'package:formvalidation/src/preferencias_usuario/preferencias_usuario.dart';
 import 'package:http/http.dart' as http;
 
 class UsuarioProvider {
   final String _firebaseToken = 'AIzaSyCc0YAuQHMX5BIpm5crR8SfjRySswVxNpQ';
+  final _prefs = new PreferenciasUsuario();
 
   Future<Map<String, dynamic>> login(String email, String password) async {
     final authData = {
@@ -20,6 +22,7 @@ class UsuarioProvider {
     print(decodedResp);
 
     if (decodedResp.containsKey('idToken')) {
+      _prefs.token = decodedResp['idToken'];
       return {'ok': true, 'token': decodedResp['idToken']};
     } else {
       return {'ok': false, 'mensaje': decodedResp['error']['message']};
@@ -42,6 +45,8 @@ class UsuarioProvider {
     print(decodedResp);
 
     if (decodedResp.containsKey('idToken')) {
+      _prefs.token = decodedResp['idToken'];
+
       return {'ok': true, 'token': decodedResp['idToken']};
     } else {
       return {'ok': false, 'mensaje': decodedResp['error']['message']};
