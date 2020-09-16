23520261f9f590b324afb59566e9f9666c8d2285
Leonardo Porras <leopg17@gmail.com>
2020-09-16 16:02:32 -0600

    Añadir segundo bloc en inheritedWidget

--- lib/src/bloc/provider.dart
+++ lib/src/bloc/provider.dart
@@ -1,34 +1,42 @@
 import 'package:flutter/material.dart';
+
 import 'package:formvalidation/src/bloc/login_bloc.dart';
 export 'package:formvalidation/src/bloc/login_bloc.dart';
 
+import 'package:formvalidation/src/bloc/productos_bloc.dart';
+export 'package:formvalidation/src/bloc/productos_bloc.dart';
+
 class Provider extends InheritedWidget {
+  final loginBloc = new LoginBloc();
+
+  final _productosBloc = new ProductosBloc();
 
   static Provider _instancia;
 
-  factory Provider({Key key, Widget child}){
-    if (_instancia == null){
+  factory Provider({Key key, Widget child}) {
+    if (_instancia == null) {
       _instancia = new Provider._internal(key: key, child: child);
     }
     return _instancia;
   }
 
-  Provider._internal({Key key, Widget child})
-     :  super(key: key, child: child);
+  Provider._internal({Key key, Widget child}) : super(key: key, child: child);
 
-  final loginBloc = LoginBloc();
-
-
-  //Constructor 
+  //Constructor
   // Provider({Key key, Widget child})
   //   :  super(key: key, child: child);
 
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) => true;
 
-  static LoginBloc of (BuildContext context) {
+  static LoginBloc of(BuildContext context) {
     return context.dependOnInheritedWidgetOfExactType<Provider>().loginBloc;
+    //Buscar en el arbol de widget y tratarlo como la clase Provider
   }
 
-
+  static ProductosBloc productosBloc(BuildContext context) {
+    return context
+        .dependOnInheritedWidgetOfExactType<Provider>()
+        ._productosBloc;
+  }
 }
